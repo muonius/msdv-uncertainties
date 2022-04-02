b2b176a13fdfec14ff18548e655f901a394ce0e8
Yang Zhao <freda.y.zhao@gmail.com>
2022-04-02 19:32:43 -0400

    update

---
+++ prototype-02/README.md
@@ -0,0 +1,5 @@
+Particles
+
+Plinko
+
+Buckets
--- prototype-02/plinko.js
+++ prototype-02/plinko.js
@@ -2,6 +2,24 @@
 //Part 2: https://www.youtube.com/watch?v=6s4MJcUyaUE&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=81
 //Part 3: https://www.youtube.com/watch?v=jN-sW-SxNzk&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=82&t=52s
 
-function setup() {}
+const Engine = Matter.Engine,
+  World = Matter.World,
+  Bodies = Matter.Bodies;
 
-function draw() {}
+let engine,
+  world,
+  particles = [];
+
+function setup() {
+  createCanvas(600, 400);
+  engine = Engine.create();
+  world = engine.world;
+
+  let p = new Particle(300, 50, 20);
+  particles.push(p);
+}
+
+function draw() {
+  background(50);
+  particles[0].show();
+}
