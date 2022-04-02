5938d106380c5d065014db24203a9cefe79794cd
Yang Zhao <freda.y.zhao@gmail.com>
2022-04-02 17:29:32 -0400

    plinko

---
+++ prototype-02/plinko.js
@@ -0,0 +1,7 @@
+//Part 1: https://www.youtube.com/watch?v=KakpnfDv_f0&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=80&t=1370s
+//Part 2: https://www.youtube.com/watch?v=6s4MJcUyaUE&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=81
+//Part 3: https://www.youtube.com/watch?v=jN-sW-SxNzk&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=82&t=52s
+
+function setup() {}
+
+function draw() {}
