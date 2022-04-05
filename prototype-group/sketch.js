3c22bcc0a3e6bfc9d346c68186f14671ce80ad35
Yang Zhao <freda.y.zhao@gmail.com>
2022-04-05 09:44:07 -0400

    init prototype

--- prototype-group/sketch.js
+++ prototype-group/sketch.js
@@ -1,6 +1,9 @@
+//credit: https://compform.net/js_lab/js_lab.html?https://sketches2022spring.compform.net/posts/WgfwTaX6jXH5QXuqm/code
+
 const pathPoints = 80;
 const paths = 29;
-const distance = 400 / paths;
+const distOffset = 1;
+const distance = 420 / paths - distOffset;
 
 const bgColor = "black";
 const lineColor = "white";
@@ -12,14 +15,14 @@ const lineHeights = [];
 let renderCount = 0;
 let renderCountIncrement = 0.2;
 const renderCountMax = 200;
-const maxAmplitude = 200;
+const maxAmplitude = 150;
 const baseLineAmplitude = 4;
 
 function setup() {
   createCanvas(800, 600);
   lineWidth = width * 0.5;
 
-  frameRate(15);
+  frameRate(10);
   stroke(lineColor);
   noFill();
 
@@ -34,15 +37,24 @@ function draw() {
   translate(width / 2 - lineWidth * 0.78, height / 5);
 
   for (let i = 0; i < paths; i++) {
-    translate(distance / 2, distance);
+    translate(distance / 2 + 0.1 * i, distance);
+    //text
+    push();
+    fill(colorSwitch(i));
+    noStroke();
+    textSize(10);
+    text(`Team ${i + 1}`, distance / 2 - 1 * i, distance);
+    pop();
 
+    //line
     beginShape();
     for (let p = 0; p < pathPoints; p++) {
+      noFill();
       let pRatio = p / pathPoints;
 
       let pX = pRatio * lineWidth;
 
-      let multiplicator = (sin(pRatio * TWO_PI - HALF_PI) + 1) / 2;
+      let multiplicator = (sin(pRatio * TWO_PI - PI * 0.8) + 1) / 2;
 
       multiplicator = pow(multiplicator, 6);
 
@@ -52,9 +64,9 @@ function draw() {
       // noiseDetail(lineHeights[i], p );
 
       let pY =
-        noise(lineHeights[i] * 0.1, p * 0.08) * multiplicator * maxAmplitude;
-      pY += (noise(p, lineHeights[i]) - 0.5) * baseLineAmplitude;
-
+        noise(lineHeights[i] * 0.1, p * 0.02) * multiplicator * maxAmplitude;
+      pY += (noise(p, lineHeights[i]) - 0.8) * baseLineAmplitude;
+      fill(colorSwitch(i));
       vertex(pX, -pY);
     }
     endShape();
@@ -68,5 +80,17 @@ function draw() {
     renderCountIncrement = -1;
   } else if (renderCount <= 0) {
     renderCountIncrement = 1;
+  }
+}
+
+function colorSwitch(i) {
+  if (i <= 5) {
+    return "purple";
+  } else if (i <= 13) {
+    return "green";
+  } else if (i <= 25) {
+    return "steelblue";
+  } else {
+    return "beige";
   }
 }
