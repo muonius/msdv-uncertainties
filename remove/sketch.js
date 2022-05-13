41364badb00173ce760d8022a19f831af025d6da
Yang Zhao <freda.y.zhao@gmail.com>
2022-05-13 13:29:30 -0400

    update

--- remove/sketch.js
+++ remove/sketch.js
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=EyG_2AdHlzY
 let bubbles = [];
 function setup() {
   createCanvas(600, 400);
@@ -6,10 +7,34 @@ function setup() {
   }
 }
 
+function mouseDragged() {
+  bubbles.push(new Bubble(mouseX, mouseY));
+}
+
+function keyPressed() {
+  bubbles.splice(0, 1);
+  //remove element in the order they were added;
+}
+
 function draw() {
   background(0);
   for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].move();
     bubbles[i].display();
   }
+}
+
+function Bubble(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.display = function () {
+    stroke(255);
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  };
+  this.move = function () {
+    this.x = this.x + random(-1, 1);
+    this.y = this.y + random(-1, 1);
+  };
 }
